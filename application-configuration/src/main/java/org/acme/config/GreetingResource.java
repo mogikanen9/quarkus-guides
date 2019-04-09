2fc64e290f49dbc0d69fca60deecd013c08551de
Vlad Voinov <mogikanensoftware@gmail.com>
2019-04-09 11:50:21 -0400

    properties added to resource class and config

--- application-configuration/src/main/java/org/acme/config/GreetingResource.java
+++ application-configuration/src/main/java/org/acme/config/GreetingResource.java
@@ -1,16 +1,29 @@
 package org.acme.config;
 
+import java.util.Optional;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 @Path("/greeting")
 public class GreetingResource {
+
+    @ConfigProperty(name = "greeting.message")
+    private String message;
+
+    @ConfigProperty(name = "greeting.suffix", defaultValue = "!")
+    private String suffix;
+
+    @ConfigProperty(name = "greeting.name")
+    private Optional<String> name;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "hello";
+        return message + " " + name.orElse("world") + suffix;
     }
 }
