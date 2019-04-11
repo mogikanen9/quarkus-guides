00317297c872eadcce6298eeaafaed3ce3140de3
Vlad Voinov <mogikanensoftware@gmail.com>
2019-04-11 10:54:03 -0400

    init

---
+++ hibernate-orm/src/test/java/org/acme/persistence/NativeSantaClausResourceIT.java
@@ -0,0 +1,9 @@
+package org.acme.persistence;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeSantaClausResourceIT extends SantaClausResourceTest {
+
+    // Execute the same tests but in native mode.
+}
